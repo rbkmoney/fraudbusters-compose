8317ddc73ebc9da03ec156c95f9c6d0af692be0f
k.struzhkin <k.struzhkin@MacBook-Pro-rbkadmin.local>
2020-07-08 13:27:05 +0300

    Add new graf and fix user for CH

--- clickhouse/docker-entrypoint-initdb.d/init-db.sh
+++ clickhouse/docker-entrypoint-initdb.d/init-db.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 set -e
 
+CLICKHOUSE_USER="${CLICKHOUSE_USER:-user}";
+CLICKHOUSE_PASSWORD="${CLICKHOUSE_PASSWORD:-password}";
+
+cat <<EOT >> /etc/clickhouse-server/users.d/user.xml
+<yandex>
+  <!-- Docs: <https://clickhouse.tech/docs/en/operations/settings/settings_users/> -->
+  <users>
+    <${CLICKHOUSE_USER}>
+      <profile>default</profile>
+      <networks>
+        <ip>::/0</ip>
+      </networks>
+      <password>${CLICKHOUSE_PASSWORD}</password>
+      <quota>default</quota>
+    </${CLICKHOUSE_USER}>
+  </users>
+</yandex>
+EOT
+
 clickhouse client -n <<-EOSQL
     CREATE DATABASE IF NOT EXISTS fraud;
 
