52c58063ec4a3f31979609b05a5381abd0029061
ggmaleva <griwania@gmail.com>
2021-09-06 16:46:18 +0300

    add analytic schema in clickhouse

--- clickhouse/docker-entrypoint-initdb.d/init-db.sh
+++ clickhouse/docker-entrypoint-initdb.d/init-db.sh
@@ -602,4 +602,168 @@ clickhouse client -n <<-EOSQL
     VALUES
     ('2019-12-05', 1587761208, 1587759200000, 'group_1', '2035728', 'email', '4bef59146f8e4640ab34915f84ddac8b', 5000, 'RUB', 'failed', '666', '1DkratTHbpg.1', '204.26.61.110', '666', '3125', 'bank_card', '477bba133c182267fe5f086924abdc5db71f77bfc27f01f2843f2cdc69d89f05', 'VISA', '123', '1', 'RUS', 'Error', '1111vbtt');
 
+
+    CREATE DATABASE IF NOT EXISTS analytic;
+
+    DROP TABLE IF EXISTS analytic.events_sink;
+
+    create table analytic.events_sink
+    (
+        timestamp             Date,
+        eventTime             UInt64,
+        eventTimeHour         UInt64,
+
+        partyId               String,
+        shopId                String,
+
+        email                 String,
+        providerName          String,
+
+        amount                UInt64,
+        guaranteeDeposit      UInt64,
+        systemFee             UInt64,
+        providerFee           UInt64,
+        externalFee           UInt64,
+        currency              String,
+
+        status                Enum8('pending' = 1, 'processed' = 2, 'captured' = 3, 'cancelled' = 4, 'failed' = 5),
+        errorReason           String,
+        errorCode             String,
+
+        invoiceId             String,
+        paymentId             String,
+        sequenceId            UInt64,
+
+        ip                    String,
+        bin                   String,
+        maskedPan             String,
+        paymentTool           String,
+        fingerprint           String,
+        cardToken             String,
+        paymentSystem         String,
+        digitalWalletProvider String,
+        digitalWalletToken    String,
+        cryptoCurrency        String,
+        mobileOperator        String,
+
+        paymentCountry        String,
+        bankCountry           String,
+
+        paymentTime           UInt64,
+        providerId            String,
+        terminal              String
+    ) ENGINE = ReplacingMergeTree()
+    PARTITION BY toYYYYMM (timestamp)
+    ORDER BY (eventTimeHour, partyId, shopId, paymentTool, status, currency, providerName, fingerprint, cardToken, invoiceId, paymentId, sequenceId);
+
+    DROP TABLE IF EXISTS analytic.events_sink_refund;
+
+    create table analytic.events_sink_refund
+    (
+        timestamp             Date,
+        eventTime             UInt64,
+        eventTimeHour         UInt64,
+
+        partyId               String,
+        shopId                String,
+
+        email                 String,
+        providerName          String,
+
+        amount                UInt64,
+        guaranteeDeposit      UInt64,
+        systemFee             UInt64,
+        providerFee           UInt64,
+        externalFee           UInt64,
+        currency              String,
+
+        reason                String,
+
+        status                Enum8('pending' = 1, 'succeeded' = 2, 'failed' = 3),
+        errorReason           String,
+        errorCode             String,
+
+        invoiceId             String,
+        refundId              String,
+        paymentId             String,
+        sequenceId            UInt64,
+
+        ip                    String,
+        fingerprint           String,
+        cardToken             String,
+        paymentSystem         String,
+        digitalWalletProvider String,
+        digitalWalletToken    String,
+        cryptoCurrency        String,
+        mobileOperator        String,
+
+        paymentCountry        String,
+        bankCountry           String,
+
+        paymentTime           UInt64,
+        providerId            String,
+        terminal              String
+    ) ENGINE = ReplacingMergeTree()
+    PARTITION BY toYYYYMM (timestamp)
+    ORDER BY (eventTimeHour, partyId, shopId, status, currency, providerName, fingerprint, cardToken, invoiceId, paymentId, refundId, sequenceId);
+
+    DROP TABLE IF EXISTS analytic.events_sink_adjustment;
+
+    create table analytic.events_sink_adjustment
+    (
+        timestamp             Date,
+        eventTime             UInt64,
+        eventTimeHour         UInt64,
+
+        partyId               String,
+        shopId                String,
+
+        email                 String,
+        providerName          String,
+
+        amount                UInt64,
+        guaranteeDeposit      UInt64,
+        systemFee             UInt64,
+        providerFee           UInt64,
+        externalFee           UInt64,
+
+        oldAmount             UInt64,
+        oldGuaranteeDeposit   UInt64,
+        oldSystemFee          UInt64,
+        oldProviderFee        UInt64,
+        oldExternalFee        UInt64,
+
+        currency              String,
+
+        reason                String,
+
+        status                Enum8('captured' = 1, 'cancelled' = 2),
+        errorCode             String,
+        errorReason           String,
+
+        invoiceId             String,
+        adjustmentId          String,
+        paymentId             String,
+        sequenceId            UInt64,
+
+        ip                    String,
+        fingerprint           String,
+        cardToken             String,
+        paymentSystem         String,
+        digitalWalletProvider String,
+        digitalWalletToken    String,
+        cryptoCurrency        String,
+        mobileOperator        String,
+
+        paymentCountry        String,
+        bankCountry           String,
+
+        paymentTime           UInt64,
+        providerId            String,
+        terminal              String
+
+    ) ENGINE = ReplacingMergeTree()
+    PARTITION BY toYYYYMM (timestamp)
+    ORDER BY (eventTimeHour, partyId, shopId, status, currency, providerName, fingerprint, cardToken, invoiceId, paymentId, adjustmentId, sequenceId);
+
 EOSQL
